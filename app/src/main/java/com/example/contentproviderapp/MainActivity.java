e2192216421be1a0e6fec4ad8419a28a236e9faa
Micoder-dev <micoder.com@gmail.com>
2022-07-28 00:10:10 +0530

    Content Provider App! - Access the contacts

--- app/src/main/java/com/example/contentproviderapp/MainActivity.java
+++ app/src/main/java/com/example/contentproviderapp/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.contentproviderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imgContacts;
+
+    private static final int RCREQUESTCODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgContacts = findViewById(R.id.imgContacts);
+
+        imgContacts.setOnClickListener(view -> {
+
+            askForReadContactsPermission();
+
+        });
+
+    }
+
+    private void askForReadContactsPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, RCREQUESTCODE);
+
+        } else {
+
+            askForContacts();
+
+        }
+
+    }
+
+    private void askForContacts() {
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+        //to get no of contacts
+        Log.i("ContentProvider", "No of contacts is : " + cursor.getCount());
+
+        //to get & display the name and number from the contacts
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                Log.i("ContentProvider", "Name is: " + name + " and number is: " + number);
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+            case RCREQUESTCODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    askForContacts();
+
+                } else {
+
+
+
+                }
+
+                return;
+
+        }
     }
 }
